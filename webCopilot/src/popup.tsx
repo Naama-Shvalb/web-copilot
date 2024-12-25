77070c27b7da1426b04f111b88c30e4087974b2e
naamashvalb <naamah.shv@gmail.com>
2024-12-25 18:29:44 +0200

    remove unused folder2

--- webCopilot/src/popup.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import PopupComponent from './PopupComponent';
-
-const root = createRoot(document.getElementById('root')!);
-root.render(<PopupComponent />);
